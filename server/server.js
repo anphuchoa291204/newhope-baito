4481167b2ae6584f5aeb1db465f4dc7455829801
anphuchoa291204 <142864120+hoapooh@users.noreply.github.com>
2024-12-07 14:32:58 +0700

    Trigger deployment

--- server/server.js
+++ server/server.js
@@ -68,6 +68,8 @@ app.all("*", (req, res, next) => {
 
 app.use(globalErrorHandler)
 
+// CHECKPOINT: 7. Start the server
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
 	console.log("Server is running on port 8080")
